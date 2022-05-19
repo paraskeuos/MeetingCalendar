6fdd6215c8f60f6f7aad536ad731dd45b7045753
paraskeuos <milan.radisic@gmail.com>
2022-05-19 18:06:41 +0200

    Add month and year display on load for Calendar component

--- meeting-calendar/src/components/Calendar.js
+++ meeting-calendar/src/components/Calendar.js
@@ -1,8 +1,17 @@
-
+import { useState, useEffect } from 'react';
 
 const Calendar = () => {
+    const [month, setMonth] = useState('');
+    const [year, setYear] = useState(0);
+    
+    useEffect(() => {
+        const date = new Date();
+        setMonth(date.toLocaleString('default', { month: 'long' }));
+        setYear(1900 + date.getYear());
+    }, []);
+
     return (
-        <div>Calendar</div>
+        <div>{ month + ' ' + year }</div>
     );
 };
 
