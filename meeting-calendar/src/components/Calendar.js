4926aa6a1d778c81b47cf01f613b2b471735b26c
paraskeuos <milan.radisic@gmail.com>
2022-05-19 18:12:28 +0200

    Change month/year to heading and add short description

--- meeting-calendar/src/components/Calendar.js
+++ meeting-calendar/src/components/Calendar.js
@@ -11,7 +11,12 @@ const Calendar = () => {
     }, []);
 
     return (
-        <div>{ month + ' ' + year }</div>
+        <>
+            <h1>{ month + ' ' + year }</h1>
+            <p>Here are all your planned events. 
+                You will find information for each event as well as plan a new one.
+            </p>
+        </>
     );
 };
 
