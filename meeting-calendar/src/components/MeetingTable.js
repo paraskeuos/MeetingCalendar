9eab6116d720319589c470eb80858a447939d282
paraskeuos <milan.radisic@gmail.com>
2022-06-19 14:12:53 +0200

    Add tmp global table css

--- meeting-calendar/src/components/MeetingTable.js
+++ meeting-calendar/src/components/MeetingTable.js
@@ -1,7 +1,6 @@
 import { useState, useEffect } from 'react';
 
 const MeetingTable = ({month, itemsPerWeek}) => {
-    const [cellsPerRow, setCellsPerRow] = useState(0);
 
     return (
         <table>
