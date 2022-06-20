b754531e73a534c461f5f48bb50d9dc761e1ccd0
paraskeuos <milan.radisic@gmail.com>
2022-06-20 14:11:31 +0200

    Add addMeeting code to API

--- server/components/meeting/meetingsController.js
+++ server/components/meeting/meetingsController.js
@@ -1,7 +1,27 @@
+const mongoose = require('mongoose');
 const Meeting = require('./meetingModel');
+const Participant = require('../participant/participantModel');
 
 module.exports.addMeeting = async (req, res, next) => {
-    // TODO
+    try {
+        const participants = [];
+        req.body.participants.array.forEach(participantName => {
+            const participant = Participant.find({ name: participantName });
+            participants.push(participant);
+        });
+
+        const meetingObj = new Meeting({
+            _id: new mongoose.Types.ObjectId(),
+            name: req.body.name,
+            description: req.body.description,
+            time: req.body.time,
+            participants
+        });
+
+        res.status(201).json({ msg: 'Meeting added' });
+    } catch(err) {
+        next(err);
+    }
 };
 
 module.exports.deleteMeeting = async (req, res, next) => {
