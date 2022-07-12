784999a2c4038fcce6d35325f9a813410a322c89
paraskeuos <milan.radisic@gmail.com>
2022-07-12 09:16:26 +0200

    Move components .js files to separate folders

--- meeting-calendar/src/components/Calendar/index.js
+++ meeting-calendar/src/components/Calendar/index.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
-import MeetingTable from './MeetingTable';
-import Day from '../components/Day';
+import MeetingTable from '../MeetingTable';
+import Day from '../Day';
 
 const Calendar = () => {
     const [monthName, setMonthName] = useState('');
--- meeting-calendar/src/components/MeetingTable/index.js
+++ meeting-calendar/src/components/MeetingTable/index.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { Day } from '../components/Day';
+import { Day } from '../Day';
 
 const MeetingTable = ({month, itemsPerWeek}) => {
 
