f722a9d480e72f2a23ee486f5b3930f04cbf6957
paraskeuos <milan.radisic@gmail.com>
2022-07-12 10:38:59 +0200

    Switch to using controlled form components

--- meeting-calendar/src/components/AddMeetingModal/index.js
+++ meeting-calendar/src/components/AddMeetingModal/index.js
@@ -1,48 +1,60 @@
 import styles from './addmeeting.module.css';
-import { useRef } from 'react';
+import { useState } from 'react';
 
 const AddMeetingModal = ({ participants }) => {
-    const title = useRef();
-    const desc = useRef();
-    const time = useRef();
-    const attendees = useRef();
+    const [meetingInfo, setMeetingInfo] = useState({});
+
+    const handleChange = (e) => {
+        const newInfo = {...meetingInfo};
+        if(e.target.name === 'attendees') {
+            const options = e.target.options;
+            newInfo.participants = [];
+            for(let i=0; i<options.length; i++) {
+                if(options[i].selected) {
+                    newInfo.participants.push(options[i].value);
+                }
+            }
+        } else {
+            newInfo[e.target.name] = e.target.value;
+        }
+        setMeetingInfo(newInfo);
+    };
 
     return (
         <div className={styles.overlay}>
             <div className={styles.modal}>
                 <form onSubmit={(e) => {
-                    const multipleSelect = e.target[3].options;
-                    const selectedAttendees = [];
-                    for(let i=0; i<multipleSelect.length; i++) {
-                        if(multipleSelect[i].selected) {
-                            selectedAttendees.push(multipleSelect[i].text);
-                        }
-                    }
-
-                    const meetingInfo = {
-                        title: title.current.value,
-                        description: desc.current.value,
-                        time: time.current.value,
-                        participants: selectedAttendees
-                    };
                     console.log(meetingInfo);
                     e.preventDefault();
                 }}>
                     <div className={styles.input}>
                         <label>Title:</label>
-                        <input type="text" required ref={title}/>
+                        <input type="text"
+                               name="title"
+                               onChange={(e) => handleChange(e)}
+                               required />
                     </div>
                     <div className={styles.input}>
                         <label>Description:</label>
-                        <textarea type="text" required ref={desc}/>
+                        <textarea type="text"
+                                  name="description"
+                                  onChange={(e) => handleChange(e)}
+                                  required />
                     </div>
                     <div className={styles.input}>
                         <label>Time:</label>
-                        <input type="text" required ref={time}/>
+                        <input type="text"
+                               name="time"
+                               onChange={(e) => handleChange(e)}
+                               required />
                     </div>
                     <div className={styles.input}>
                         <label>Participants:</label>
-                        <select size="5" multiple required ref={attendees}>
+                        <select size="5"
+                                name="attendees"
+                                onChange={(e) => handleChange(e)}
+                                multiple
+                                required >
                             { participants?.map((participant, partId) => 
                                 <option key={partId}>{participant.name}</option>) }
                         </select>
