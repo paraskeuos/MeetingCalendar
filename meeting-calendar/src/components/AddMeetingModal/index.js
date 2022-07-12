90e08dda09375cec59ddcfdbcacff99a21a2425a
paraskeuos <milan.radisic@gmail.com>
2022-07-12 10:15:40 +0200

    Add temp onSubmit code for meetingInfo creation

--- meeting-calendar/src/components/AddMeetingModal/index.js
+++ meeting-calendar/src/components/AddMeetingModal/index.js
@@ -1,25 +1,48 @@
 import styles from './addmeeting.module.css';
+import { useRef } from 'react';
 
 const AddMeetingModal = ({ participants }) => {
+    const title = useRef();
+    const desc = useRef();
+    const time = useRef();
+    const attendees = useRef();
+
     return (
         <div className={styles.overlay}>
             <div className={styles.modal}>
-                <form>
+                <form onSubmit={(e) => {
+                    const multipleSelect = e.target[3].options;
+                    const selectedAttendees = [];
+                    for(let i=0; i<multipleSelect.length; i++) {
+                        if(multipleSelect[i].selected) {
+                            selectedAttendees.push(multipleSelect[i].text);
+                        }
+                    }
+
+                    const meetingInfo = {
+                        title: title.current.value,
+                        description: desc.current.value,
+                        time: time.current.value,
+                        participants: selectedAttendees
+                    };
+                    console.log(meetingInfo);
+                    e.preventDefault();
+                }}>
                     <div className={styles.input}>
                         <label>Title:</label>
-                        <input type="text" />
+                        <input type="text" required ref={title}/>
                     </div>
                     <div className={styles.input}>
                         <label>Description:</label>
-                        <textarea type="text" />
+                        <textarea type="text" required ref={desc}/>
                     </div>
                     <div className={styles.input}>
                         <label>Time:</label>
-                        <input type="text" />
+                        <input type="text" required ref={time}/>
                     </div>
                     <div className={styles.input}>
                         <label>Participants:</label>
-                        <select size="5" multiple>
+                        <select size="5" multiple required ref={attendees}>
                             { participants?.map((participant, partId) => 
                                 <option key={partId}>{participant.name}</option>) }
                         </select>
