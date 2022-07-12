7b0559d17d48ab2836ccdd64d4888473181c4d9d
paraskeuos <milan.radisic@gmail.com>
2022-07-12 09:48:36 +0200

    Add form input elements

--- meeting-calendar/src/components/AddMeetingModal/index.js
+++ meeting-calendar/src/components/AddMeetingModal/index.js
@@ -3,7 +3,31 @@ import styles from './addmeeting.module.css';
 const AddMeetingModal = () => {
     return (
         <div className={styles.overlay}>
-            <div className={styles.modal}>Modal</div>
+            <div className={styles.modal}>
+                <form>
+                    <div className={styles.input}>
+                        <label>Title:</label>
+                        <input type="text" />
+                    </div>
+                    <div className={styles.input}>
+                        <label>Description:</label>
+                        <textarea type="text" />
+                    </div>
+                    <div className={styles.input}>
+                        <label>Time:</label>
+                        <input type="text" />
+                    </div>
+                    <div className={styles.input}>
+                        <label>Participants:</label>
+                        <select size="5" multiple>
+                            <option>Pera</option>
+                            <option>Mika</option>
+                            <option>Zika</option>
+                        </select>
+                    </div>
+                    <button type="submit">Save</button>
+                </form>
+            </div>
         </div>
     );
 };
