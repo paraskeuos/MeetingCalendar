243e86c1e63be5dcaef0d195e543bc95731127f4
paraskeuos <milan.radisic@gmail.com>
2022-07-13 09:50:34 +0200

    Add styling to sastanak

--- meeting-calendar/pages/sastanak/[:meetId].js
+++ meeting-calendar/pages/sastanak/[:meetId].js
@@ -1,22 +1,25 @@
 import Link from 'next/link';
 import useRouter from 'next/router';
+import styles from './sastanak.module.css'
 
 const MeetingInfo = () => {
     const router = useRouter.useRouter();
 
     return (
-        <>
-        <p>{ router.query.name }</p>
-        <p>{ router.query.time }</p>
+        <div className={styles.body}>
+        <h2 className={styles.item}>{ router.query.name }</h2>
+        <p id={styles.time}>{ router.query.time }</p>
         <p>{ router.query.description }</p>
-        <ul>
+        <ul id={styles.list}>
             Participants:
             { router.query.participants.split(',').map(
                 (participant, pId) => (<li key={pId}>{participant}</li>)
                 
             )}
         </ul>
-        <button onClick={() => {
+        
+        <div className={styles.buttondiv}>
+        <button className={styles.button} id={styles.remove} onClick={() => {
             const reqObj = {
                 method: 'DELETE',
                 headers: {
@@ -28,11 +31,13 @@ const MeetingInfo = () => {
             fetch('http://localhost:4000/meetings/', reqObj)
             .then(res => res.json())
             .then(json => router.push('/'));
-        }}>Remove meeting</button>
+        }}>DELETE</button>
+        
         <Link href="/">
-            <a>Back</a>
+            <button className={styles.button}>Back</button>
         </Link>
-        </>
+        </div>
+        </div>
     );
 };
 
